230994b64ad94a28c8271db261b2fc8cc18c64ff
Evgenii Rtishchev <e.rtishchev@gmail.com>
2019-03-15 12:48:31 +0300

    Fixed Encoding section

--- AwesomeICUString.playground/Contents.swift
+++ AwesomeICUString.playground/Contents.swift
@@ -56,7 +56,7 @@ print("🌍".applyingTransform(.toUnicodeName, reverse: false) ?? "🌍")
 
 extension String {
 	func transformed(with rule: String, reverse: Bool = false) -> String {
-		return self.applyingTransform(StringTransform(rawValue: rule), reverse: false) ?? self
+		return self.applyingTransform(StringTransform(rawValue: rule), reverse: reverse) ?? self
 	}
 }
 
@@ -142,15 +142,33 @@ print(stringWithDigits.components(separatedBy: CharacterSet.decimalDigits).joine
 
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// Filtering
+// Encoding
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-print("&<>'\"".transformed(with: "Hex/XML"))
+// You've definetely had this problem if you worked with XML data from backend.
+// You received an encoded string and need to get the non-escaped symbols.
+// There are 5 special XML characters: &<>'"
+
 let xmlString = "&#x26;&#x3C;&#x3E;&#x27;&#x22;"
+
+// You may use code like that:
+
+print(xmlString
+	.replacingOccurrences(of: "&#x26;", with: "&")
+	.replacingOccurrences(of: "&#x3C;", with: "<")
+	.replacingOccurrences(of: "&#x3E;", with: ">")
+	.replacingOccurrences(of: "&#x27;", with: "'")
+	.replacingOccurrences(of: "&#x22;", with: "\"")
+)
+
+// Or you may use power of NSAttributedString, but please read carefully about performance problems and limitations:
+// https://developer.apple.com/documentation/foundation/nsattributedstring/1524613-init
 
 let data = xmlString.data(using: .utf8)!
 let attributedString = try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
 print(attributedString.string)
+
+// Or you may use powers of ICU transforms:
 
 print(xmlString.transformed(with: "Hex/XML", reverse: true))
 
